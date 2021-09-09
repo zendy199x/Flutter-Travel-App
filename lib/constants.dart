9baa629b1fe91bacdfa5b8d9552e61a8a7454bc7
Zendy <nguyenduc.051194@gmail.com>
2021-09-09 23:40:02 +0700

    Setup project

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF3E4067);
+const Color kTextColor = Color(0xFF3F4168);
+const Color kIconColor = Color(0xFF5E5E5E);
+
+const double kDefaultPadding = 20.0;
+
+final BoxShadow kDefualtShadow = BoxShadow(
+  offset: Offset(5, 5),
+  blurRadius: 10,
+  color: Color(0xFFE9E9E9).withOpacity(0.56),
+);
